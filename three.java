be01d04d4b47701180dcf294a89570ee9d5525ec
kumar45 <vamsikrishnapaladugu1@gmail.com>
2011-11-05 12:39:10 +0530

    three.java

---
+++ three.java
@@ -0,0 +1,16 @@
+class  readme
+{
+
+public String getString(){
+
+return "srinivas";
+}
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		readme me = new readme();
+		System.out.println("the name is"+me.getString());
+		
+	}
+}
