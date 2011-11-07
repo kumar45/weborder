8b03d83200818e18e3e8c6776a0f6cf6f2430e74
kumar45 <vamsikrishnapaladugu1@gmail.com>
2011-11-07 10:32:32 +0530

    commit readme.java in master branch

--- readme.java
+++ readme.java
@@ -3,5 +3,7 @@ class  readme
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+System.out.println("Welcome to Cgvak");
+System.out.println("Location is Combatore");
 	}
 }
